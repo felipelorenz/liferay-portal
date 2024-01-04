1f8be41aa6c4fe747824675764f54efe31413ef5
Felipe Lorenz <felipe.lorenz@liferay.com>
2024-01-04 15:43:22 -0300

    LPS-203746 portal-search-api: Rename interface
    https://liferay.atlassian.net/browse/LPS-203746

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/site/SiteJSONObjectProvider.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/site/SiteJSONObjectProvider.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -15,7 +15,7 @@ import javax.portlet.ResourceRequest;
  * @author Gustavo Lima
  */
 @ProviderType
-public interface SitesJSONObjectProvider {
+public interface SiteJSONObjectProvider {
 
 	public JSONObject getSiteJSONObject(ResourceRequest resourceRequest)
 		throws Exception;
