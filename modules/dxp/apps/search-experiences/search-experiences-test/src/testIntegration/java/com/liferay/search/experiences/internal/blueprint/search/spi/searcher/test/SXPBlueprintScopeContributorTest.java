4efb130a8985f67b7a8f14efc5d6ae7b39168339
Felipe Lorenz <felipe.lorenz@hotmail.com>
2026-02-11 10:35:57 -0300

    LPD-78876 search-experiences-test: Quarantine SXPBlueprintScopeContributorTest
    https://liferay.atlassian.net/browse/LPD-78876

--- modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/blueprint/search/spi/searcher/test/SXPBlueprintScopeContributorTest.java
+++ modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/blueprint/search/spi/searcher/test/SXPBlueprintScopeContributorTest.java
@@ -47,6 +47,7 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -54,6 +55,7 @@ import org.junit.runner.RunWith;
 /**
  * @author Joshua Cords
  */
+@Ignore
 @RunWith(Arquillian.class)
 public class SXPBlueprintScopeContributorTest {
 
